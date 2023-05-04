7fd64f264f75fc9168d9e0605daadf195c5b7ec6
Rohan Ramchand <rohan@neon.xyz>
2023-05-04 10:31:04 -0700

    Pokemons -> PokemonList

--- app/page.js
+++ app/page.js
@@ -1,15 +1,15 @@
 import { sql } from "@vercel/postgres";
-import { Pokemons, Pokemon } from "./components";
+import { PokemonList, Pokemon } from "./components";
 
 export default async function Home() {
   const { rows } = await sql`SELECT * FROM pokemon ORDER BY RANDOM() LIMIT 12`;
 
   return (
-    <Pokemons>
+    <PokemonList>
       {rows.map((p) => (
         <Pokemon key={p.id} id={p.id} name={p.name} />
       ))}
-    </Pokemons>
+    </PokemonList>
   );
 }
 
