a22e495f8516095c736e720da60fee1a05982360
Malte Ubl <malte.ubl@gmail.com>
2023-11-06 18:28:25 -0800

    No loading.tsx

--- app/page.js
+++ app/page.js
@@ -1,8 +1,17 @@
 import { sql } from "@vercel/postgres";
 import { PokemonList, Pokemon } from "./components";
 import { cookies } from "next/headers";
+import { Suspense } from "react";
 
 export default async function Home() {
+  return (
+    <Suspense fallback={<Loading />}>
+      <Content />
+    </Suspense>
+  );
+}
+
+async function Content() {
   cookies(); // force dynamic
   const { rows } = await sql`SELECT * FROM pokemon ORDER BY RANDOM() LIMIT 12`;
 
@@ -12,5 +21,23 @@ export default async function Home() {
         <Pokemon key={p.id} id={p.id} name={p.name} />
       ))}
     </PokemonList>
+  );
+}
+
+function Loading() {
+  return (
+    <ul className="max-w-md flex flex-wrap justify-center gap-4 p-4 m-3">
+      {[...Array(12)].map((_, i) => (
+        <li
+          key={i}
+          className="flex flex-col items-center justify-center border bg-white border-gray-400 dark:bg-gray-700 dark:border-gray-500 p-3"
+        >
+          <div style={{ width: 96, height: 96 }} />
+          <span aria-hidden className="invisible">
+            Loading
+          </span>
+        </li>
+      ))}
+    </ul>
   );
 }
